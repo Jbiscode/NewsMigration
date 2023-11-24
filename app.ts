c2c30ec815ebbf9191c31854aef82bf2ad0c88f5
사재빈 <code6995@gmail.com>
2023-11-24 14:34:57 +0900

    mixin

--- app.ts
+++ app.ts
@@ -34,12 +34,43 @@ const store: Store= {
   feeds: [],
 };
 
-function getData<AjaxResponse>(url: string): AjaxResponse {
-  ajax.open('GET', url, false);
-  ajax.send();
+function applyApiMixins(targetClass: any, baseClasses: any[]): void{
+  baseClasses.forEach(baseClass => {
+    Object.getOwnPropertyNames(baseClass.prototype).forEach(name => {
+      const descriptor = Object.getOwnPropertyDescriptor(baseClass.prototype, name);
 
-  return JSON.parse(ajax.response);
+      if(descriptor){
+        Object.defineProperty(targetClass.prototype, name, descriptor);
+      }
+    });
+  });
 }
+class Api{
+  getRequest<AjaxResponse>(url: string): AjaxResponse{
+    const ajax = new XMLHttpRequest();
+    ajax.open('GET', url, false);
+    ajax.send();
+
+    return JSON.parse(ajax.response);
+  }
+}
+class NewsFeedApi {
+  getData(): NewsFeed[]{
+    return this.getRequest<NewsFeed[]>(NEWS_URL);
+  }
+}
+
+class NewsDetailApi {
+  getData(id:string): NewsDetail{
+    return this.getRequest<NewsDetail>(CONTENT_URL.replace('@id', id));
+  }
+}
+
+interface NewsFeedApi extends Api{}
+interface NewsDetailApi extends Api{}
+
+applyApiMixins(NewsFeedApi, [Api]);
+applyApiMixins(NewsDetailApi, [Api]);
 
 function makeFeeds(feeds: NewsFeed[]) : NewsFeed[]{
   for (let i = 0; i < feeds.length; i++) {
@@ -58,6 +89,7 @@ function updateView(html: string) : void {
 }
 
 function newsFeed() : void{
+  const api = new NewsFeedApi();
   let newsFeed: NewsFeed[] = store.feeds;
   const newsList:string[] = [];
   let template = `
@@ -86,7 +118,7 @@ function newsFeed() : void{
   `;
 
   if (newsFeed.length === 0) {
-    newsFeed = store.feeds = makeFeeds(getData<NewsFeed[]>(NEWS_URL));
+    newsFeed = store.feeds = makeFeeds(api.getData());
   }
 
   for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
@@ -120,7 +152,8 @@ function newsFeed() : void{
 
 function newsDetail() {
   const id = location.hash.substr(7);
-  const newsContent = getData<NewsDetail>(CONTENT_URL.replace('@id', id))
+  const api = new NewsDetailApi();
+  const newsContent = api.getData(id);
   let template = `
     <div class="bg-gray-600 min-h-screen pb-8">
       <div class="bg-white text-xl">
